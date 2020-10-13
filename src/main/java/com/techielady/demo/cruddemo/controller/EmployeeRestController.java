75a9cead0ae9726093e5177a05d594dcf42bcf63
Ife5983 <modulehelen44@gmail.com>
2020-10-13 04:34:19 -0700

    Changes to be committed:

---
+++ src/main/java/com/techielady/demo/cruddemo/controller/EmployeeRestController.java
@@ -0,0 +1,28 @@
+package com.techielady.demo.cruddemo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.techielady.demo.cruddemo.entity.Employee;
+import com.techielady.demo.cruddemo.services.EmployeeService;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+	
+	private EmployeeService employeeService;
+	
+	@Autowired
+	public EmployeeRestController(EmployeeService theEmployeeService) {
+		employeeService = theEmployeeService;
+	}
+	
+	@GetMapping("/employees")
+	public List<Employee> findAll(){
+		return employeeService.getAll();
+	}
+}
---
+++ src/main/java/com/techielady/demo/cruddemo/services/EmployeeServiceImpl.java
@@ -0,0 +1,45 @@
+package com.techielady.demo.cruddemo.services;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.techielady.demo.cruddemo.dao.EmployeeDAO;
+import com.techielady.demo.cruddemo.entity.Employee;
+
+public class EmployeeServiceImpl implements EmployeeService {
+	
+	private EmployeeDAO employeeDAO;
+	
+	@Autowired
+	public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {
+		employeeDAO = theEmployeeDAO;
+	}
+
+	@Override
+	@Transactional
+	public List<Employee> getAll() {
+		return employeeDAO.getAll();
+	}
+
+	@Override
+	@Transactional
+	public Employee getEmployeebyId(int theId) {
+		return employeeDAO.getEmployeebyId(theId);
+	}
+
+	@Override
+	@Transactional
+	public void saveAll(Employee thEmployee) {
+		employeeDAO.saveAll(thEmployee);
+	}
+
+	@Override
+	@Transactional
+	public void deleteById(int theId) {
+		employeeDAO.deleteById(theId);
+	}
+
+}
